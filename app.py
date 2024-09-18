c424caecab1b7ae76aa5ff1116bdf3d8e6294fdf
sewon <sewonlog@gmail.com>
2024-09-19 00:41:47 +0900

    feat : 크롬으로 사이트 연결

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 import time
-from selenium import webdriver
+from selenium import webdriver #동적 사이트 수집
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium import webdriver as wb
 
-service = Service('C:/Users/seong/chromedriver.exe')
+# wb.Chrome()
+
+# 크롬드라이버 경로 설정
+service = Service('C:/Users/sewon/chromedriver-win64/chromedriver.exe')
 service.start()
 
-driver = webdriver.Remote(service.service_url)
+# 드라이버 설정 (Remote가 아닌 Chrome을 사용)
+driver = webdriver.Chrome(service=service)
 
-login_url = "https://ticket.interpark.com/Gate/TPLogin.asp"
+# 로그인 페이지로 이동
+login_url = "https://accounts.interpark.com/login/form"
 driver.get(login_url)
 
+# 페이지 로드 대기
 time.sleep(3)
+
+# 브라우저 종료
 driver.quit()
