f74e8bd2420d2ef442c46d30223f05b1d559c64e
sewon <sewonlog@gmail.com>
2024-09-24 03:10:41 +0900

    feat : 새버전 추가

--- app2.py
+++ app2.py
@@ -11,16 +11,17 @@ import time
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 
-
 driver = webdriver.Chrome(options=chrome_options)
-#driver.set_window_size(1900, 1000)
-#driver.manage().window().maximize()
-yse24_rul = 'https://tickets.interpark.com/'
+
+#화면 크기에 맞게 윈도우창 풀사이즈로 조절
+driver.maximize_window()
+
+page_url = 'https://tickets.interpark.com/'
 
 
 # 웹페이지가 로드될 때까지 2초를 대기
 driver.implicitly_wait(time_to_wait=2)
-driver.get(url=yse24_rul)
+driver.get(url=page_url)
 
 # 스크롤 내리기
 # driver.find_element(By.TAG_NAME,'body').send_keys(Keys.PAGE_DOWN)
@@ -29,9 +30,9 @@ driver.get(url=yse24_rul)
 driver.find_element(By.XPATH,'//a[@title="로그인"]').click()
 
 userId = driver.find_element(By.ID, 'userId')
-userId.send_keys('아이디')
+userId.send_keys('id')
 userPwd = driver.find_element(By.ID, "userPwd")
-userPwd.send_keys('비번')
+userPwd.send_keys('pw')
 userPwd.send_keys(Keys.ENTER)
 
 
@@ -42,5 +43,27 @@ driver.get('https://ticket.interpark.com/Contents/Sports/GoodsInfo?SportsCode=07
 #driver.find_element(By.CLASS_NAME, 'blind').click()
 #driver.find_element(By.XPATH,'//*[@id="mainForm"]/div[9]/div/div[4]/a[4]').click()
 
+# 스크롤 내리기
+driver.find_element(By.TAG_NAME,'body').send_keys(Keys.PAGE_DOWN)
+
 # 예매하기 버튼 클릭
-driver.find_element(By.XPATH, "//a[@onclick=\"CallWynsBooking('True','Y','Y','N','24012119','','20240923','001','','','111087390','abfe07d785ee8ac465ad9b64ce3f6b77','1727057496048','N','N')\"]")
+driver.find_element(By.CLASS_NAME, 'BtnColor_Y').click()
+
+# 새로운 탭으로 이동
+print(driver.window_handles)
+driver.switch_to.window(driver.window_handles[-1])
+
+# 아이프레임으로 이동
+driver.switch_to.frame(driver.find_element(By.XPATH, "//*[@id='ifrmSeat']"))
+
+# 7초 기다림
+time.sleep(9)
+
+#팝업 닫기
+driver.find_element(By.XPATH, "//a[@href=\"javascript:fnBookNoticeShowHide('');\"]").click()
+
+#안심예매창 닫기
+# driver.find_element(By.XPATH, "//a[@onclick='capchaHide()']").click()
+
+
+
